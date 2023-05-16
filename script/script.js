84c260627f1acb89889adf30e129914342db225b
Nawaksam <samuel-cardonnel_student2023@wilder.school>
2023-05-16 18:42:13 +0200

    Js still not fixed

--- script/script.js
+++ script/script.js
@@ -1,39 +1,39 @@
+//importer la chocolatine
 const choco = document.querySelector("#choco");
 
-const updateScore = (chocolatines) => {
-  const title = document.querySelector("title");
-  const score = document.querySelector("#score span");
+// //récupérer la sauvegarde du compte
+// const getStorage = () => {
+//   const chocolatines = localStorage.getItem("chocolatines") || 0;
+//   const powerups = JSON.parse(localStorage.getItem("powerups")) || [];
+//   const storage = {
+//     chocolatines: chocolatines,
+//     powerups: powerups,
+//   };
+//   return storage;
+// };
 
-  score.innerText = chocolatines;
-  title.innerHTML = chocolatines + " - Choco Clicker";
+// //mettre à jour l'affichache
+// const updateScore = (chocolatines) => {
+//   const title = document.querySelector("title");
+//   const score = document.querySelector("#score span");
+//   score.innerText = chocolatines;
+//   title.innerHTML = chocolatines + " - Choco Clicker";
+//   localStorage.setItem("chocolatines", chocolatines);
+// };
 
-  localStorage.setItem("chocolatines", chocolatines);
-};
+// //quand choco est cliquée
+// const chocoClicked = (chocolatines) => {
+//   const storage = getStorage();
+//   const score = document.querySelector("#score span");
+//   const scoreValue = chocolatines ? chocolatines : parseInt(score.innerText);
+//   let newScore;
+//   newScore = scoreValue + 1;
 
-const getStorage = () => {
-  const chocolatines = localStorage.getItem("chocolatines") || 0;
-  const powerups = JSON.parse(localStorage.getItem("powerups")) || [];
+//   updateScore(newScore);
+// };
 
-  const storage = {
-    chocolatines: chocolatines,
-    powerups: powerups,
-  };
-
-  return storage;
-};
-
-const chocoClicked = (chocolatines) => {
-  const storage = getStorage();
-
-  const score = document.querySelector("#score span");
-  const scoreValue = chocolatines ? chocolatines : parseInt(score.innerText);
-
-  let newScore;
-  newScore = scoreValue + 1;
-
-  updateScore(newScore);
-};
-
+//écoute si on clique sur choco
 choco.addEventListener("click", () => {
-  chocoClicked();
+  //   chocoClicked();
+  console.log("Je clique");
 });
